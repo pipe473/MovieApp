bbbee3112847131666eee8a638dc898387cb377b
jupegarnica <jupegarnica@gmail.com>
2019-05-09 11:49:39 +0200

    routing working and boilerplate view created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    NavigationComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
