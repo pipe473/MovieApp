d82eb214b95ee80e9c3c1c9e73096b632a9f754e
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-10 13:39:17 +0200

    genero un deploy para mi pagina de moviesapp

--- src/app/api-movies.service.ts
+++ src/app/api-movies.service.ts
@@ -8,7 +8,7 @@ export class ApiMoviesService {
   urlBase = `https://api.themoviedb.org/3/`;
   key = '323112ea2281b9eb70f319f4df422c6b';
   generateUrl = (_type) => this.urlBase + `movie/${_type}?api_key=` + this.key;
-  // generateSimilar = (type) => this.urlBase + `movie/${id}?api_id=` + this.id;
+  
 
   constructor(private http: HttpClient){
   }
